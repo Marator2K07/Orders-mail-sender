690ceb8ae81e54d06e6ff74d1f47a0acc1bd5627
Marator2K07 <dmitriyshaaa@gmail.com>
2024-11-30 20:25:12 +0300

    Создан шаблон части сообщения с информацией о заказе.

---
+++ resources/views/emails/order_details.blade.php
@@ -0,0 +1,13 @@
+<p>Номер заказа: {{ $data['order']['id'] }}</p>
+<p>Дата: {{ \Carbon\Carbon::parse($data['order']['date'])->format('d.m.Y H:i:s') }}</p>
+<p>Статус: {{ $data['order']['status'] }}</p>
+
+@if ($data['order']['status'] !== 'completed' && $data['order']['status'] !== 'sent')
+    <p>Товары:</p>
+    <ul>
+        @foreach ($data['order']['products'] as $product)
+            <li>{{ $product['title'] }}: {{ $product['quantity'] }} x {{ $product['price'] }} = {{ $product['quantity'] * $product['price'] }}</li>
+        @endforeach
+    </ul>
+    <p>Общая сумма: {{ array_sum(array_map(function ($product) { return $product['quantity'] * $product['price']; }, $data['order']['products'])) }}</p>
+@endif
